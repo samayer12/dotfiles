66b653cc3d7b52b036fa4a556081b90e78e0f1ae
Sam Mayer <samayer12@gmail.com>
2021-12-20 17:59:03 -0600

    Generalize package installation

--- _helpers.sh
+++ _helpers.sh
@@ -6,6 +6,7 @@ NORMAL='\033[0m'
 RED='\033[0;31m'
 WHITE='\033[1;37m'
 YELLOW='\033[1;33m'
+
 function prompt() {
   echo -en "$1"
   read -p " (y/n) " -n 1 -r || exit 1; echo
@@ -14,8 +15,38 @@ function prompt() {
   else return 1 # false
   fi
 }
+
 function echo_and_run() {
   echo -e "$GREEN`pwd`$WHITE\$ $@" $NORMAL
   "$@" || { echoerr -e $RED"Looks like that didn't work.$YELLOW ¯\_(ツ)_/¯ $NORMAL"; exit 1; }
 }
 echoerr() { echo "$@" 1>&2; }
+
+function install_package(){
+
+  declare -A INSTALLERS=( 
+      [apt]="sudo apt install -y"
+      [brew]="brew install"
+      [dnf]="sudo dnf install --assume yes"
+      [pip]="pip install"
+
+)
+    if [[ $# -ne 2 ]]; then
+        echo "Specify package installer and package to install"
+        echo "Received $#, $*"
+        exit 1
+    fi
+
+    package=$1
+    installer=$2
+
+    if ! [[ ${INSTALLERS[$installer]} && $(command -v "$installer") ]];
+    then
+        echo "$installer is an unsupported installer."
+        echo "Either _helpers.sh needs to know about it in INSTALLERS"
+        echo "or it is not installed on this system."
+    fi
+    
+    echo "Install $package with $installer"
+    echo "$package" | xargs -I _ ${INSTALLERS[$installer]} _
+}
